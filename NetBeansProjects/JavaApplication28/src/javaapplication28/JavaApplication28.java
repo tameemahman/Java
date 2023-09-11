9f6ee24d8be4f85594214eea683faa8f11ae3bb5
Tameem <tameem.handb@gmail.com>
2023-09-11 12:48:41 +0600

    11.09.2023

---
+++ NetBeansProjects/JavaApplication28/src/javaapplication28/JavaApplication28.java
@@ -0,0 +1,25 @@
+
+package javaapplication28;
+
+import view.Form;
+
+
+public class JavaApplication28 {
+
+    public static void main(String[] args) {
+      
+        
+        
+        
+        
+          Form f  =new Form();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        
+        
+        
+        
+        
+    }
+    
+}
